11d9a147443f29a35984e012c7c16f9770b7761b
yeolevinit <yeolevinit24@gmail.com>
2026-03-01 21:50:05 +0530

    resolving error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,22 +4,19 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(express.json())
-app.use(cookieParser())
+// ✅ CORS first
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
 
-/* require all the routes here */
+app.use(express.json())
+app.use(cookieParser())
+
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
